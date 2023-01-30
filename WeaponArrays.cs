400f50328bf9252a4a322546fcc2fff424d915d4
Jason Kimoto <jkimoto@nevada.unr.edu>
2023-01-29 16:56:25 -0800

    Added WeaponArray.cs, initialized array and dictionary for penitence

---
+++ WeaponArrays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalLobCorpTTK
+{
+    public static class WeaponArrays
+    {
+        
+        public readonly static String[] Penitence = new string[5] { "Penitence", "Zayin", "5", "7", "0.5" };
+        public readonly static Dictionary<string, string[]> penitence = new()
+        {
+            {"Penitence", Penitence }
+        };
+        
+        
+    }
+}
