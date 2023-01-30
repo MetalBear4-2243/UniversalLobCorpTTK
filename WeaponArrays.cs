cadff4f1313382d9b8830ad8ba5eac6ce549af40
1103-Kimoto-Jason <jkimoto@nevada.unr.edu>
2023-01-29 21:59:08 -0800

    e

--- WeaponArrays.cs
+++ WeaponArrays.cs
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,32 @@ namespace UniversalLobCorpTTK
         };
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalLobCorpTTK
+{
+    public static class WeaponArrays
+    {
+        
+        public readonly static String[] Penitence = new string[6] { "Penitence", "Zayin", "White", "5", "7", "0.5" };
+        public readonly static Dictionary<string, string[]> penitence = new()
+        {
+            {"Penitence", Penitence }
+        };
+
+        public readonly static String[] Soda = new string[6] { "Soda", "Zayin", "Red", "1", "2", "0.5" };
+        public readonly static Dictionary<string, string[]> soda = new()
+        {
+            {"Soda", Soda }
+        };
+    }
+}
+>>>>>>> d494823b88bd3b6e3c86ac0556efcc77e3058603
 =======
 ﻿using System;
 using System.Collections.Generic;
